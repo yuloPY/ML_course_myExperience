cc6ba9e98d6055f63d01fad2a22fd333ed046996
Erhan <erhankeklikci910@gmail.com>
2024-08-17 18:32:19 +0300

    DeepLearning

---
+++ DeepLearning/ANN.py
@@ -0,0 +1,38 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# Data Preprocessing
+
+# Importing dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:,3:-1].values
+y = dataset.iloc[:,-1].values
+
+# Encoding categorical data
+
+# Label encoding for the 'Gender' column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:,2] = le.fit_transform(X[:,2])
+
+# One Hot Encoding 'Geography' column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[1])]
+,remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Building the ANN
