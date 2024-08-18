911cfb91a76d7c224f76e35520abf6e139a9edf2
Erhan <erhankeklikci910@gmail.com>
2024-08-18 13:16:04 +0300

    DeepLearning

--- DeepLearning/ANN.py
+++ DeepLearning/ANN.py
@@ -36,3 +36,33 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Building the ANN
+
+# Initializing the ANN
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=6,activation='relu'))
+
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=6,activation='relu'))
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1,activation='sigmoid'))
+
+# Compiling ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training ANN on the training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Predicting the test set results
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy = accuracy_score(y_test, y_pred)
+
+print(accuracy)
